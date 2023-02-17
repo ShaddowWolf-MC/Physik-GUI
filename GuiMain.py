df1abffb64e6e6ee01f116d093e7e0e639e5b8de
ShaddowWolf-MC <78350407+ShaddowWolf-MC@users.noreply.github.com>
2023-02-17 22:10:02 +0100

    Update GuiMain.py

--- GuiMain.py
+++ GuiMain.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import StringVar
+from tkinter import Tk
+from tkinter import OptionMenu
+#from tkinter import *
 import serial
 import serial.tools.list_ports
 import sys
@@ -14,11 +17,17 @@ print("Python Version", sys.version)
 
 
 class MyGUI:
-    def __init__(self):
-        
+    def __init__(self, parent):
+        Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+
         self.window = tk.Tk()
         self.window.minsize(500, 500)
+        self.window.title("Physik GUI Shit")
 
+        self.option_var = tk.StringVar()
 
         comlist = serial.tools.list_ports.comports()
         connected = []
@@ -26,8 +35,9 @@ class MyGUI:
             connected.append(element.device)
         print("Connected COM ports: " + str(connected))
 
-        defaultComDD = StringVar(self.window)
-        defaultComDD.set("Choose COMport")
+        self.optionsTest = {'One' : 1, 'Two' : 2, 'Three' : 3}
+        #defaultComDD = StringVar(self.window)
+        #defaultComDD.set("Choose COMport")
 
         defaultBaudrateDD = StringVar(self.window)
         defaultBaudrateDD.set("Choose Baudrate")
@@ -44,7 +54,9 @@ class MyGUI:
         btn1 = tk.Button(layoutframe, text='1', font=('Arial', 20))
         btn1.grid(row=0, column=0, sticky=tk.W)
         
-        dropdown1 = tk.OptionMenu(self.window, defaultComDD, *comlist)
+        
+        #dropdown1 = tk.OptionMenu(self.window, defaultComDD, *comlist, command=ParseValues, )
+        #dropdown1 = ttk.OptionMenu(self, self.option_var, *self.optionsTest.keys(), command=self.option_changed)
         dropdown1.grid(row=2, column=2, padx=60)
 
         dropdown2 = tk.OptionMenu(self.window, defaultBaudrateDD, *baudrates)
@@ -54,6 +66,13 @@ class MyGUI:
                                    "If unknown, use 115200!", font=('Calibre', 10))
         commentBaudrate.grid(row=3, column=5, padx=40)
 
+        
+        buttonSet = tk.Button(self.window, text="Set Values", font=('Arial', 13), command=self.SetValuesGUI)
+        buttonSet.grid(row=4, column=4)
+
+
+
+        
         #layoutframe.pack(fill='x')
 
         #self.label = tk.Label(self.window, text="Your Message", font=('Arial', 18))
@@ -75,7 +94,33 @@ class MyGUI:
 
         self.window.mainloop()
 
-    def show_message(self):
+        
+
+    def initialize(self):
+       
+        optionList = ["Yes","No"]
+        self.dropVar=StringVar()
+        self.dropVar.set("Yes") # default choice
+        self.dropMenu1 = OptionMenu(self, self.dropVar, *optionList,
+                                    command=self.func)
+        self.dropMenu1.grid(column=1,row=4)
+
+    def func(self,value):
+        print(value)
+    
+    def option_changed(self, *args):
+        # get the selected option string
+        selected = self.option_var.get()
+        # get the value from the options dictionary
+        self.output_label['text'] = f'You selected: {self.options[selected]}'
+
+
+
+    def SetValuesGUI(self):
+
+     #   self.
+
+
         if self.check_state.get() == 0:
             #when starting in the beginning of the Textbox we need index 1 as a string, and index to as the end
             #also: the index starts at fucking 1, why???
