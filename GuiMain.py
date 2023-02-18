f86b4de6075fb2819481099ad58d976d71cf467a
ShaddowWolf-MC <78350407+ShaddowWolf-MC@users.noreply.github.com>
2023-02-18 22:37:13 +0100

    Added possibility to get values from selected OptionMenu todo: change variable names

--- GuiMain.py
+++ GuiMain.py
@@ -18,6 +18,8 @@ class MyGUI:
         
         self.window = tk.Tk()
         self.window.minsize(500, 500)
+        self.window.title("Physics GUI Window - Dev_Version")
+        #Set window, window size and window title
 
 
         comlist = serial.tools.list_ports.comports()
@@ -28,12 +30,13 @@ class MyGUI:
         #get all conected com ports
 
 
-        defaultComDD = StringVar(self.window)
-        defaultComDD.set("Choose COMport")
+        self.defaultComDD = tk.StringVar(self.window)
+        self.defaultComDD.set("Choose COMport")
         #string for Dropdown menu
+        #self because otherwise it doesnt work, idk why?
 
-        defaultBaudrateDD = StringVar(self.window)
-        defaultBaudrateDD.set("Choose Baudrate")
+        self.defaultBaudrateDD = StringVar(self.window)
+        self.defaultBaudrateDD.set("Choose Baudrate")
         baudrates = [300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 31250, 38400, 57600, 115200]
         #Possible baudrates for the Arduino, should usually be 115200
 
@@ -47,20 +50,26 @@ class MyGUI:
         #colums for my gui, will utilise later
 
 
-        btn1 = tk.Button(layoutframe, text='1', font=('Arial', 20))
-        btn1.grid(row=0, column=0, sticky=tk.W)
+        btn1 = tk.Button(self.window, text='Set Port / baudrate', font=('Arial', 13), command=lambda: self.my_show())
+        btn1.grid(row=2, column=5)
         
-        dropdown1 = tk.OptionMenu(self.window, defaultComDD, *comlist)
+        dropdown1 = tk.OptionMenu(self.window, self.defaultComDD, *comlist)
         dropdown1.grid(row=2, column=2, padx=60)
         #dropdown for choosing the comport
 
-        dropdown2 = tk.OptionMenu(self.window, defaultBaudrateDD, *baudrates)
-        dropdown2.grid(row=2, column=5, padx=40)
+        dropdown2 = tk.OptionMenu(self.window, self.defaultBaudrateDD, *baudrates)
+        dropdown2.grid(row=2, column=4, padx=40)
         #dropdown for choosing the baudrate
 
         commentBaudrate = tk.Label(self.window, text="Baudrate must be same as \n configured in Arduino code \n\n" 
                                    "If unknown, use 115200!", font=('Calibre', 10))
         commentBaudrate.grid(row=3, column=5, padx=40)
+
+
+
+        str_out=tk.StringVar(self.window)
+        str_out.set("Output")
+
 
         #layoutframe.pack(fill='x')
 
@@ -82,6 +91,18 @@ class MyGUI:
 
 
         self.window.mainloop()
+
+    def my_show(self):
+        #self.str_out.set(self.defaultComDD.get())
+        print(self.defaultComDD.get())
+        self.defaultComDD = self.defaultComDD.get()
+        print(self.defaultComDD)
+        print(self.defaultBaudrateDD.get())
+        self.defaultBaudrateDD = self.defaultBaudrateDD.get()
+        print(self.defaultBaudrateDD)
+        #Changes the value of defaultComDD and defaultBaudrateDD to the selected values
+        
+
 
     def show_message(self):
         if self.check_state.get() == 0:
